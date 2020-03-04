43656ee5faf6d35f47082058f3271456104cffcf
Jota <clarinetejorge@gmail.com>
2020-03-04 14:37:53 +0100

    Player scripts started

--- FPS - Proyectos IV/Assets/Scripts/Player/RbFPSController.cs
+++ FPS - Proyectos IV/Assets/Scripts/Player/RbFPSController.cs
@@ -2,8 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
 public class RbFPSController : MonoBehaviour
 {
+    [System.Serializable]
+    public class MovementSettings
+    {
+        public float ForwardSpeed = 8f;
+        public float BackwardSpeed = 4f;
+        public float StrafeSpeed = 4f;
+        public float RunMultiplier = 2f;
+
+        public float JumpForce = 30f;
+        [HideInInspector] public float CurrentTargetSpeed = 8f;
+
+        private bool _Running;
+
+        public bool Running { get => _Running; }
+
+        public void UpdateDesiredTargetSpeed(Vector2 input)
+        {
+            if (input == Vector2.zero) return;
+            if (input.x > 0 || input.x < 0)
+            {
+                CurrentTargetSpeed = StrafeSpeed;
+            }
+            if (input.y < 0)
+            {
+                //backwards
+                CurrentTargetSpeed = BackwardSpeed;
+            }
+            if (input.y > 0)
+            {
+                //forwards
+                //handled last as if strafing and moving forward at the same time forwards speed should take precedence
+                CurrentTargetSpeed = ForwardSpeed;
+            }
+            if (_Running)
+            {
+                CurrentTargetSpeed *= RunMultiplier;
+            }
+        }
+    }
+
+    [System.Serializable]
+    public class AdvancedSettings
+    {
+        public float groundCheckDistance = 0.01f; // distance for checking if the controller is grounded ( 0.01f seems to work best for this )
+        public float stickToGroundHelperDistance = 0.5f; // stops the character
+        public float slowDownRate = 20f; // rate at which the controller comes to a stop when there is no input
+        public bool airControl; // can the user control the direction that is being moved in the air
+        [Tooltip("set it to 0.1 or more if you get stuck in wall")]
+        public float shellOffset; //reduce the radius by that ratio to avoid getting stuck in wall (a value of 0.1f is nice)
+    }
+
+    public Camera cam;
+    public MovementSettings movementSettings = new MovementSettings();
+    public 
     // Start is called before the first frame update
     void Start()
     {
