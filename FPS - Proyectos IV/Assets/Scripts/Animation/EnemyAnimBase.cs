044b15216a6e31ad83a7bcebbbd42304ad8a8b08
asteroide <asteroideconpatas@gmail.com>
2020-04-03 18:17:33 +0200

    First level without props. Animations WIP

---
+++ FPS - Proyectos IV/Assets/Scripts/Animation/EnemyAnimBase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAnimBase : MonoBehaviour
+{
+    Animator cmpAnimator;
+    NavMeshAgent cmpAgent;
+
+    public float interpolationSpeed = 3;
+    float animatorSpeed;
+
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        cmpAnimator = this.GetComponent<Animator>();
+        cmpAgent = this.GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        float agentSpeed = cmpAgent.velocity.magnitude;
+        animatorSpeed = Mathf.MoveTowards(animatorSpeed, agentSpeed, interpolationSpeed * Time.deltaTime);
+        cmpAnimator.SetFloat("Speed", animatorSpeed);
+    }
+}
