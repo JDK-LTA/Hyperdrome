c10f05758305314242a4d7829f0d8b86b1ed5127
Jota García García <jotagamedev@gmail.com>
2020-05-30 04:10:37 +0200

    Spikes coded and running smoothly (I think)

---
+++ FPS - Proyectos IV/Assets/Scripts/Spike.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Spike : MonoBehaviour
+{
+    private void SpikeEnd()
+    {
+        GetComponentInParent<SpikeParent>().DisableSpike();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GameManager.Instance.PlayerTakeHit(GetComponentInParent<SpikeParent>().damage);
+        }
+    }
+}
