93bbf1950428561720356e532eb500cb68cb8717
Jota García García <jotagamedev@gmail.com>
2020-06-02 23:29:16 +0200

    Auto stash before merge of "master" and "origin/master"

--- FPS - Proyectos IV/Assets/Scripts/Weapons/ShotTypes/ShotLock.cs
+++ FPS - Proyectos IV/Assets/Scripts/Weapons/ShotTypes/ShotLock.cs
@@ -7,27 +7,27 @@ public class ShotLock : ShotSemi
     // FALTA EL TEMA DE LA ANIMACIÓN. NO SÉ SI HACER COINCIDIR EL TIEMPO
     //DE ANIMACIÓN A MANO CON EL TIEMPO DE CD O INTENTAR PROGRAMARLO
     //EN CONJUNTO
-    bool canStartShooting = false;
-    protected override void Start()
-    {
-        base.Start();
-        transform.GetComponentInParent<WeaponAnimator>().LockFinished += IfNotShootingCanShoot;
-    }
-
-    protected override void Update()
-    {
-        if (!Input.GetMouseButton(0) && !canStartShooting)
-        {
-            canStartShooting = true;
-        }
-        if (canShoot && canStartShooting)
-        {
-            if (shooting)
-            {
-                weapon.Shoot();
-                canShoot = false;
-            }
-        }
+    bool canStartShooting = false;
+    protected override void Start()
+    {
+        base.Start();
+        transform.GetComponentInParent<WeaponAnimator>().LockFinished += IfNotShootingCanShoot;
+    }
+
+    protected override void Update()
+    {
+        if (!Input.GetMouseButton(0) && !canStartShooting)
+        {
+            canStartShooting = true;
+        }
+        if (canShoot && canStartShooting)
+        {
+            if (shooting)
+            {
+                weapon.Shoot();
+                canShoot = false;
+            }
+        }
     }
 
     protected override void IfNotShootingCanShoot()
